c2caa28b6f4823e7d83b6e662f6093eb462efadf
walkerhilla <huny98.dev@gmail.com>
2023-09-10 16:18:53 +0900

    BaseCollectionViewCell

---
+++ ShoppingApp/View/BaseCollectionViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  BaseCollectionViewCell.swift
+//  ShoppingApp
+//
+//  Created by walkerhilla on 2023/09/09.
+//
+
+import UIKit
+import SnapKit
+
+class BaseCollectionViewCell: UICollectionViewCell {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setConstains() { }
+  
+  func configureView() { }
+}
