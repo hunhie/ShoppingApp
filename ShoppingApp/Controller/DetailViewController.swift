9c167a68c254685dd00dd53e3e5b3685362cd6c8
walkerhilla <huny98.dev@gmail.com>
2023-09-11 21:51:59 +0900

    DetailViewController 수정

--- ShoppingApp/Controller/DetailViewController.swift
+++ ShoppingApp/Controller/DetailViewController.swift
@@ -16,9 +16,6 @@ final class DetailViewController: BaseViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "heart.fill"), style: .plain, target: self, action: #selector(likeButtonTapped))
-    navigationItem.rightBarButtonItem?.tintColor = .white
-    
     guard let data else { return }
     title = data.title.htmlEscaped
     let url = URL(string: "https://msearch.shopping.naver.com/product/\(data.productID)")
@@ -37,9 +34,5 @@ final class DetailViewController: BaseViewController {
     webView.snp.makeConstraints { make in
       make.edges.equalToSuperview()
     }
-  }
-  
-  @objc func likeButtonTapped() {
-    
   }
 }
