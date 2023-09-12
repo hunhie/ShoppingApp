2c21c21af960af74a2df401950192309f89db168
walkerhilla <huny98.dev@gmail.com>
2023-09-12 23:16:28 +0900

    스크롤 시 키보드 내리기 구현

--- ShoppingApp/Extension/UIVIewController+Extension.swift
+++ ShoppingApp/Extension/UIVIewController+Extension.swift
@@ -21,12 +21,19 @@ extension UIViewController {
     return lastParent?.view.window?.windowScene?.screen
   }
   
-  func addTapGestureForEndEditing() {
-//    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapScreen))
-//    view.addGestureRecognizer(tapGestureRecognizer)
-  }
-  
-  @objc func didTapScreen() {
+  //  func addTapGestureForEndEditing() {
+  //    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(didTapScreen))
+  //    tapGestureRecognizer.cancelsTouchesInView = false
+  //    view.addGestureRecognizer(tapGestureRecognizer)
+  //  }
+  //
+  //  @objc func didTapScreen() {
+  //    view.endEditing(true)
+  //  }
+}
+
+extension UIViewController: UIScrollViewDelegate {
+  public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
     view.endEditing(true)
   }
 }
